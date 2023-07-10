7ea76edd7cba8bfb887632ac6cc51542b4be3131
dimant3 <135867231+dimant3@users.noreply.github.com>
2023-07-10 21:59:25 +0300

    1st commit

--- final_project.py
+++ final_project.py
@@ -1,3 +1,11 @@
-# start
-x = 'hello_world'
-print(x)
+import pandas as pd
+
+# formatting Nasdaq csv: date format and column split
+nasdaq = pd.read_csv('Nasdaq OMX baltic.csv')
+nasdaq['Date'] = pd.to_datetime(nasdaq['Date'])
+nasdaq['Date'] = nasdaq['Date'].dt.strftime('%Y-%m')
+print(nasdaq)
+
+# 2x csv failai neirasomi i postgres (tik sutvarkomi duomenys), o web scrap supumpuojam i postgres
+
+
