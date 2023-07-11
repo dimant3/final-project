743b5032c167444df558635a956d0c3c8d029aec
dimant3 <135867231+dimant3@users.noreply.github.com>
2023-07-11 18:07:57 +0300

    1st commit

---
+++ postgres.py
@@ -0,0 +1,10 @@
+# postgres connection settings
+db_settings = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'students',
+    'user': 'postgres',
+    'password': 'barabanas12'
+}
+
+
