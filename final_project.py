4a52e7a5a0b1b89070977c8f85279b6a77d9366d
dimant3 <135867231+dimant3@users.noreply.github.com>
2023-07-14 20:02:57 +0300

    9th commit

--- final_project.py
+++ final_project.py
@@ -3,6 +3,8 @@ from postgres import work_with_database
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 # formatting Nasdaq csv: date format and column split
 nasdaq = pd.read_csv('Nasdaq OMX baltic.csv')
@@ -20,18 +22,16 @@ rusell['Close'] = round(rusell['Close'], 2)
 # we needed to create a Dictionary of values
 rusell = pd.DataFrame({
     'Date': ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07',
-    '2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02',
-    '2021-03', '2021-04', '2021-05', '2021-06', '2021-07', '2021-08', '2021-09',
-    '2021-10', '2021-11', '2021-12', '2022-01', '2022-02', '2022-03', '2022-04',
-    '2022-05', '2022-06', '2022-07', '2022-08', '2022-09', '2022-10', '2022-11',
-    '2022-12'],
+             '2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02',
+             '2021-03', '2021-04', '2021-05', '2021-06', '2021-07', '2021-08', '2021-09',
+             '2021-10', '2021-11', '2021-12', '2022-01', '2022-02', '2022-03', '2022-04',
+             '2022-05', '2022-06', '2022-07', '2022-08', '2022-09', '2022-10', '2022-11',
+             '2022-12'],
     'Close': [1614.06, 1476.43, 1153.1, 1310.66, 1394.04, 1441.37, 1480.43, 1561.88, 1507.69,
-    1538.48, 1819.82, 1974.86, 2073.64, 2201.05, 2220.52, 2266.45, 2268.97, 2310.55,
-    2226.25, 2273.77, 2204.37, 2297.19, 2198.91, 2245.31, 2028.45, 2048.09, 2070.13,
-    1864.1, 1864.04, 1707.99, 1885.23, 1844.12, 1664.72, 1846.86, 1886.58, 1761.25]
+              1538.48, 1819.82, 1974.86, 2073.64, 2201.05, 2220.52, 2266.45, 2268.97, 2310.55,
+              2226.25, 2273.77, 2204.37, 2297.19, 2198.91, 2245.31, 2028.45, 2048.09, 2070.13,
+              1864.1, 1864.04, 1707.99, 1885.23, 1844.12, 1664.72, 1846.86, 1886.58, 1761.25]
 })
-
-
 
 # formatting SP500 index from postgres database with function(work_with_database) from postgres.py
 data_from_postgres = work_with_database()
@@ -84,7 +84,7 @@ sp500['Deviation from average'] = round(sp500['close'] - sp500['close'].mean(axi
 # 'close' column average for each index: nasdaq = 1244.74 , rusell = 1875.87 , sp500 = 384.84
 
 
-#Now finding how many months each INDEX closed above the average price using Boolean ( just to mention in presentation)
+# Now finding how many months each INDEX closed above the average price using Boolean ( just to mention in presentation)
 nasdaq_above_index_avg = nasdaq.value_counts(nasdaq['Close'] > nasdaq['Close'].mean())
 rusell_above_index_avg = rusell.value_counts(rusell['Close'] > rusell['Close'].mean())
 sp500_above_index_avg = sp500.value_counts(sp500['close'] > sp500['close'].mean())
@@ -104,111 +104,155 @@ lowest_sp500_value_index = np.argmin(sp500['Portfolio value'])
 
 # Here we wanted to see which INDEX has highest difference between min and max in portfolio values
 min_max_dif_nasdaq = np.ptp(nasdaq['Portfolio value'])
-min_value_nasdaq = np.min(nasdaq['Portfolio value']) # 7708.17
-max_value_nasdaq = np.max(nasdaq['Portfolio value']) # 16429.45
+min_value_nasdaq = np.min(nasdaq['Portfolio value'])  # 7708.17
+max_value_nasdaq = np.max(nasdaq['Portfolio value'])  # 16429.45
 
 min_max_dif_rusell = np.ptp(rusell['Portfolio value'])
-min_value_rusell = np.min(rusell['Portfolio value']) # 7144.1
-max_value_rusell = np.max(rusell['Portfolio value']) # 14315.14
+min_value_rusell = np.min(rusell['Portfolio value'])  # 7144.1
+max_value_rusell = np.max(rusell['Portfolio value'])  # 14315.14
 
 min_max_dif_sp500 = np.ptp(sp500['Portfolio value'])
-min_value_sp500 = np.min(sp500['Portfolio value']) # 8011.38
-max_value_sp500 = np.max(sp500['Portfolio value']) # 14762.69
+min_value_sp500 = np.min(sp500['Portfolio value'])  # 8011.38
+max_value_sp500 = np.max(sp500['Portfolio value'])  # 14762.69
+
+
 # we found out highest difference between min and max in: nasdaq = 8721.28, rusell = 7171.04 , sp500 = 6751.31
 
 
-        # In next steps we creating visualization with matplotlib and seaborn
+# In next steps we creating visualization with matplotlib and seaborn
 
-# Adding x variable for X-Axis "Date" values
-# x = rusell['Date']
+# 1. creating function to call Indexes portfolio values by month
+def show_portfolio_values():
+    # Adding x variable for X-Axis "Date" values
+    x = rusell['Date']
+    plt.figure(figsize=(14, 10))
+    plt.plot(x, nasdaq['Portfolio value'], label='Nasdaq', color='green')
+    plt.plot(rusell['Portfolio value'], label='Russell2000', color='blue')
+    plt.plot(sp500['Portfolio value'], label='SP500', color='red')
 
-# 1. creating line chart each INDEX portfolio value
-# plt.plot(x, nasdaq['Portfolio value'], label='Nasdaq', color='green')
-# plt.plot(rusell['Portfolio value'], label='Russell2000', color='blue')
-# plt.plot(sp500['Portfolio value'], label='SP500', color='red')
-#
-# # Marking the highest value points in each index
-# plt.plot(highest_nasdaq_value_index, max_value_nasdaq, marker='o', color='black', label="Highest")
-# plt.plot(lowest_nasdaq_value_index, min_value_nasdaq, marker='o', color='black', label="Lowest")
-#
-# plt.plot(highest_rusell_value_index, max_value_rusell, marker='o', color='black')
-# plt.plot(lowest_rusell_value_index, min_value_rusell, marker='o', color='black')
-#
-# plt.plot(highest_sp500_value_index, max_value_sp500, marker='o', color='black')
-# plt.plot(lowest_sp500_value_index, min_value_sp500, marker='o', color='black')
+    # Marking the highest value points in each index
+    plt.plot(highest_nasdaq_value_index, max_value_nasdaq, marker='o', color='black', label="Highest")
+    plt.plot(lowest_nasdaq_value_index, min_value_nasdaq, marker='o', color='black', label="Lowest")
 
-#add legend
-# plt.legend(title='Portfolio values by INDEX')
+    plt.plot(highest_rusell_value_index, max_value_rusell, marker='o', color='black')
+    plt.plot(lowest_rusell_value_index, min_value_rusell, marker='o', color='black')
 
-#add x and y axes with labels and a title
-# plt.ylabel('Value €', fontsize=14)
-# plt.xlabel('Date', fontsize=14)
-# plt.title('Portfolios by INDEX', fontsize=16)
-# plt.xticks(x, rotation=45)
-# plt.grid()
-# plt.show()
+    plt.plot(highest_sp500_value_index, max_value_sp500, marker='o', color='black')
+    plt.plot(lowest_sp500_value_index, min_value_sp500, marker='o', color='black')
+
+    # add legend
+    plt.legend(title='Portfolio values by INDEX')
+
+    # adding x and y axes with labels and a title
+    plt.ylabel('Value €', fontsize=14)
+    plt.xlabel('Date', fontsize=14)
+    plt.title('Portfolios by INDEX', fontsize=16)
+    plt.xticks(x, rotation=45)
+    plt.grid()
+    plt.show()
 
 
-# 2. showing total ROI for each index.
-# plt.figure(figsize=(12, 8))
-# plt.bar(total_rois['Stocks index'], total_rois['Total ROI'])
-# plt.ylabel('ROI %')
-# plt.title('Total ROI by indexes')
-# plt.rcParams.update({'font.size': 22})
-# plt.show()
+# 2. Creating function (def) to show total ROI for each index.
+def show_index_total_roi():
+    plt.figure(figsize=(12, 8))
+    plt.bar(total_rois['Stocks index'], total_rois['Total ROI'])
+    plt.ylabel('ROI %')
+    plt.title('Total ROI by indexes')
+    plt.rcParams.update({'font.size': 22})
+    plt.show()
 
-# 3. Each index graph deviation from standard.
+
+# 3. Creating function to show each index deviation from standard.
 # Nasdaq graph
-# x = rusell['Date']
-# nas = nasdaq['Deviation from average'].mean()
-# filter_data = nasdaq[nasdaq['Deviation from average'] > nas]
-# filter_data2 = nasdaq[nasdaq['Deviation from average'] < nas]
-# plt.figure(figsize=(14, 10))
-# plt.plot(x, nasdaq['Deviation from average'], label='Nasdaq', color='darkgreen')
-# plt.scatter(filter_data.index, filter_data['Deviation from average'], color='red', label='20 dots above')
-# plt.scatter(filter_data2.index, filter_data2['Deviation from average'], color='blue', label='16 dots below')
-# plt.axhline(y=nas, color='black', linestyle='--')
-# plt.xlabel('Date')
-# plt.ylabel('Deviation from average')
-# plt.title('Nasdaq deviation from average')
-# plt.legend()
-# plt.xticks(x, rotation=45)
-# plt.show()
+def show_nasdaq_deviation():
+    x = rusell['Date']
+    nas = nasdaq['Deviation from average'].mean()
+    filter_data = nasdaq[nasdaq['Deviation from average'] > nas]
+    filter_data2 = nasdaq[nasdaq['Deviation from average'] < nas]
+    plt.figure(figsize=(14, 10))
+    plt.plot(x, nasdaq['Deviation from average'], label='Nasdaq', color='darkgreen')
+    plt.scatter(filter_data.index, filter_data['Deviation from average'], color='red', label='20 dots above')
+    plt.scatter(filter_data2.index, filter_data2['Deviation from average'], color='blue', label='16 dots below')
+    plt.axhline(y=nas, color='black', linestyle='--')
+    plt.xlabel('Date')
+    plt.ylabel('Deviation from average')
+    plt.title('Nasdaq deviation from average')
+    plt.legend()
+    plt.xticks(x, rotation=45)
+    plt.show()
+
 
 # Russell 2000 graph
-# x = rusell['Date']
-# rus = rusell['Deviation from average'].mean()
-# filter_data_rus = rusell[rusell['Deviation from average'] > rus]
-# filter_data_rus2 = rusell[rusell['Deviation from average'] < rus]
-# plt.figure(figsize=(14, 10))
-# plt.plot(x, rusell['Deviation from average'], label='Russell2000', color='orange')
-# plt.scatter(filter_data_rus.index, filter_data_rus['Deviation from average'], color='red', label='18 dots above')
-# plt.scatter(filter_data_rus2.index, filter_data_rus2['Deviation from average'], color='blue', label='18 dots below')
-# plt.axhline(y=rus, color='black', linestyle='--')
-# plt.xlabel('Date')
-# plt.ylabel('Deviation from average')
-# plt.title('Russell 2000 deviation from average')
-# plt.legend()
-# plt.xticks(x, rotation=45)
-# plt.show()
+def show_russell2000_deviation():
+    x = rusell['Date']
+    rus = rusell['Deviation from average'].mean()
+    filter_data_rus = rusell[rusell['Deviation from average'] > rus]
+    filter_data_rus2 = rusell[rusell['Deviation from average'] < rus]
+    plt.figure(figsize=(14, 10))
+    plt.plot(x, rusell['Deviation from average'], label='Russell2000', color='purple')
+    plt.scatter(filter_data_rus.index, filter_data_rus['Deviation from average'], color='red', label='18 dots above')
+    plt.scatter(filter_data_rus2.index, filter_data_rus2['Deviation from average'], color='blue', label='18 dots below')
+    plt.axhline(y=rus, color='black', linestyle='--')
+    plt.xlabel('Date')
+    plt.ylabel('Deviation from average')
+    plt.title('Russell 2000 deviation from average')
+    plt.legend()
+    plt.xticks(x, rotation=45)
+    plt.show()
+
 
 # SP500 graph
-# x = rusell['Date']
-# spy = sp500['Deviation from average'].mean()
-# filter_data_spy = sp500[sp500['Deviation from average'] > spy]
-# filter_data_spy2 = sp500[sp500['Deviation from average'] < spy]
-# plt.figure(figsize=(14, 10))
-# plt.plot(x, sp500['Deviation from average'], label='SP500', color='black')
-# plt.scatter(filter_data_spy.index, filter_data_spy['Deviation from average'], color='red', label='19 dots above')
-# plt.scatter(filter_data_spy2.index, filter_data_spy2['Deviation from average'], color='blue', label='17 dots below')
-# plt.axhline(y=spy, color='black', linestyle='--')
-# plt.xlabel('Date')
-# plt.ylabel('Deviation from average')
-# plt.title('SP500 deviation from average')
-# plt.legend()
-# plt.xticks(x, rotation=45)
-# plt.show()
+def show_sp500_deviation():
+    x = rusell['Date']
+    spy = sp500['Deviation from average'].mean()
+    filter_data_spy = sp500[sp500['Deviation from average'] > spy]
+    filter_data_spy2 = sp500[sp500['Deviation from average'] < spy]
+    plt.figure(figsize=(14, 10))
+    plt.plot(x, sp500['Deviation from average'], label='SP500', color='black')
+    plt.scatter(filter_data_spy.index, filter_data_spy['Deviation from average'], color='red', label='19 dots above')
+    plt.scatter(filter_data_spy2.index, filter_data_spy2['Deviation from average'], color='blue', label='17 dots below')
+    plt.axhline(y=spy, color='black', linestyle='--')
+    plt.xlabel('Date')
+    plt.ylabel('Deviation from average')
+    plt.title('SP500 deviation from average')
+    plt.legend()
+    plt.xticks(x, rotation=45)
+    plt.show()
 
+
+# function to call 1st graph - Portfolio values by indexes
+# show_portfolio_values()
+
+# funtion to call 2nd graph - Total ROI by indexes for whole period
+# show_index_total_roi()
+
+# function to call 3rd graphs - indexes deviation vs average deviation
+# show_nasdaq_deviation()
+# show_russell2000_deviation()
+# show_sp500_deviation()
+
+
+# FORECAST
+def nasdaq_forecast():
+    # sns.set()
+    x = rusell['Date']
+    plt.plot(x, nasdaq['Close'], )
+
+    train = x[x < pd.to_datetime("2022-11", format='%Y-%m')]
+    test = x[x > pd.to_datetime("2022-11", format='%Y-%m')]
+
+    plt.plot(train, color="black")
+    plt.plot(test, color="red")
+    plt.figure(figsize=(14, 10))
+    plt.xlabel('Date')
+    plt.ylabel('Index Price')
+    plt.xticks(rotation=45)
+    plt.title('Train/test split for Nasdaq index')
+    plt.show()
+
+def nasdaq_forecast2():
+    X = nasdaq['Close']
+    y = nasdaq
 
 
 
